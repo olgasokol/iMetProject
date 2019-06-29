4a05780892bfb5750ef6086ad96ee83345dbe5e1
olga <olga.sokol456@gmail.com>
2019-06-29 04:44:05 +0200

    test evaluation of nn setup score stabilization

--- imet.py
+++ imet.py
@@ -18,23 +18,21 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
 plt.ion()  # interactive mode
-data_transforms = transforms.Compose([
+train_data_transforms = transforms.Compose([
     torchvision.transforms.ToPILImage(),
     transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-    # transforms.ToTensor()
 ])
 
+val_data_transforms = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
 
-# 'val': transforms.Compose([
-#     transforms.Resize(256),
-#     transforms.CenterCrop(224),
-#     transforms.ToTensor(),
-#     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-# ]),
-# }
 
 class IMetDataset(torch.utils.data.Dataset):
 
@@ -59,16 +57,21 @@ class IMetDataset(torch.utils.data.Dataset):
         labels = np.zeros((len(self.labels_frame)))  # self.labels_frame.iloc[label_idxs, 1].values
         labels[label_idxs] = 1
         labels = torch.DoubleTensor(labels)
-        # labels = torch.as_tensor(labels)
-        # print(labels)
         if self.transform:
             image = self.transform(image)
 
         return image, labels
 
 
-data = IMetDataset(root_dir='data/train', labels_csv='data/labels.csv', annotations_csv='data/train.csv',
-                   transform=data_transforms)
+train_data = IMetDataset(root_dir='data/train', labels_csv='data/labels.csv', annotations_csv='data/train.csv',
+                         transform=train_data_transforms)
+
+
+train_data = IMetDataset(root_dir='data/train', labels_csv='data/labels.csv', annotations_csv='data/train.csv',
+                         transform=val_data_transforms)
+
+test_data = IMetDataset(root_dir='data/test', labels_csv='data/labels.csv', annotations_csv='data/train.csv',
+                        transform=val_data_transforms)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('Using device: %s' % device)
@@ -77,57 +80,44 @@ print('Using device: %s' % device)
 # --------------------------------
 # Hyper-parameters
 # --------------------------------
-# input_size = 32 * 32 * 3
 layer_config = [512, 512]
-num_classes = len(data.labels_frame)
-# print(num_classes)
+num_classes = len(train_data.labels_frame)
 num_epochs = 30
-batch_size = 50
+batch_size = 40
 learning_rate = 1e-3
 learning_rate_decay = 0.99
 reg = 0  # 0.001
-dataset_size = len(data)
-num_training = int(dataset_size * 0.6)
+dataset_size = len(train_data)
+num_training = int(dataset_size * 0.8)
 num_validation = int(dataset_size * 0.2)
-num_test = int(dataset_size * 0.2)
 dataset_size = {'train': num_training, 'val': num_validation}
 fine_tune = True
 pretrained = True
 
 mask = list(range(num_training))
-train_dataset = tutils.dataset.Subset(data, mask)
+train_dataset = tutils.dataset.Subset(train_data, mask)
 mask = list(range(num_training, num_training + num_validation))
-val_dataset = torch.utils.data.dataset.Subset(data, mask)
-mask = list(range(num_training + num_validation, num_training + num_validation + num_test))
-test_dataset = torch.utils.data.Subset(data, mask)
+val_dataset = torch.utils.data.dataset.Subset(train_data, mask)
 
-# mask = list(range(num_training))
-# train_sampler = torch.utils.data.sampler.SubsetRandomSampler(mask)
-# mask = list(range(num_training, num_training + num_validation))
-# val_sampler = torch.utils.data.sampler.SubsetRandomSampler(mask)
-# mask = list(range(num_training + num_validation, num_training + num_validation + num_test))
-# test_sampler = torch.utils.data.sampler.SubsetRandomSampler(mask)
 
 # -------------------------------------------------
 # Data loader
 # -------------------------------------------------
-train_loader = torch.utils.data.DataLoader(dataset=data,
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
                                            shuffle=True,
                                            num_workers=4)
 
-val_loader = torch.utils.data.DataLoader(dataset=data,
+val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
                                          batch_size=batch_size,
                                          shuffle=False,
                                          num_workers=4)
 
-test_loader = torch.utils.data.DataLoader(dataset=data,
+test_loader = torch.utils.data.DataLoader(dataset=test_data,
                                           batch_size=batch_size,
                                           shuffle=False,
                                           num_workers=4)
 
-# dataloader = torch.utils.data.DataLoader(data, batch_size=25, shuffle=True, num_workers=4)
-# class_names = image_datasets['train'].classes
 loader = {'train': train_loader, 'val': val_loader}
 print("loaders done")
 
@@ -164,14 +154,9 @@ def train_model(model, optimizer, criterion,  scheduler, num_epochs):
 
                 # forward
                 # track history if only in train
-                # with .(phase == 'train'):
                 with torch.set_grad_enabled(phase == 'train'):
                     outputs = model(inputs).double()
                     loss = criterion(outputs, labels)
-                    # _, preds = torch.max(outputs, 1)
-                    # outputs = outputs/outputs[:, preds]
-                    # outputs = outputs<0.5]
-                    # ///implement correct loss
                     preds = torch.nn.Softmax(outputs)
                     preds = preds.dim > 0.5
                     preds = torch.IntTensor(preds.cpu().int())
@@ -182,7 +167,9 @@ def train_model(model, optimizer, criterion,  scheduler, num_epochs):
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()
-
+                if score_num % 100 == 0:
+                    print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                        score_num, loss, score))
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += score
@@ -210,14 +197,16 @@ def train_model(model, optimizer, criterion,  scheduler, num_epochs):
     return model
 
 
+eps = 0.0001
+
 def f2sdcore(out, target, beta=2.):
     #out - binary results
-    tp = out * target
-    fp = (out | target - target) * out
-    fn = (out | target - target) * target
-    p = tp / (tp+fp)
-    r = tp / (tp + fn)
-    score = (1+beta*beta)*p*r/(beta*beta*p + r)
+    tp = torch.sum(out * target).double()
+    fp = torch.sum((out | target - target) * out).double()
+    fn = torch.sum((out | target - target) * target).double()
+    p = tp / (tp+fp + eps)
+    r = tp / (tp + fn + eps)
+    score = (1+beta*beta)*p*r/(beta*beta*p + r + eps)
     return score
 
 model_ft = models.resnet18(pretrained=True)
@@ -240,6 +229,27 @@ exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, ga
 print("lr done")
 
 model_ft = train_model(model_ft, optimizer_ft, criterion, exp_lr_scheduler, num_epochs=num_epochs)
+print("train done")
+
+model_ft.eval()
+with torch.no_grad():
+    score_total = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model_ft(images)
+        preds = torch.nn.Softmax(outputs)
+        preds = preds.dim > 0.5
+        preds = torch.IntTensor(preds.cpu().int())
+
+        score = f2sdcore(preds, torch.IntTensor(labels.cpu().int()))
+        total += 1
+        score_total += score
+        if total == 1000:
+            break
+
+    print('Accuracy of the network on the test images: {} %'.format(score_total / total))
 
 
 def imshow(inp, title=None):
